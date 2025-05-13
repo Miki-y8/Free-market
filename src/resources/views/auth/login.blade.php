dea173581cf9685dd6702972265b1fc0408e120f
miki <mi7893451dmgjs8@gmail.com>
2025-05-13 17:52:59 +0900

    view

---
+++ src/resources/views/auth/login.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.login_register')
+
+@section('content')
+<div class="login-form">
+    <h2 class="login-form__heading">ログイン</h2>
+        <form class="login-form__form" action="/login" method="post">
+        @csrf
+
+        <div class="login-form__group">
+            <label class="login-form__label" for="email">メールアドレス</label>
+            <input class="login-form__input" type="mail" name="email" id="email">
+            <p class="register-form__error-message">
+                @error('email')
+            {{ $message }}
+            @enderror
+            </p>
+        </div>
+        <div class="login-form__group">
+            <label class="login-form__label" for="password">パスワード</label>
+            <input class="login-form__input" type="password" name="password" id="password">
+            <p>
+            @error('password')
+            {{ $message }}
+            @enderror
+            </p>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">ログインする</button>
+        </div>
+        <div class="register__link">
+            <a class="register__button-submit" href="/auth/registration">会員登録はこちら</a>
+        </div>
+</div>
+@endsection
---
+++ src/resources/views/auth/registration.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.login_register')
+
+@section('content')
+<div class="registration-form">
+    <h2 class="registration-form__heading">会員登録</h2>
+        <form class="registration-form__form" action="/login" method="post">
+        @csrf
+
+        <div class="registration-form__group">
+            <label class="registration-form__label" for="name">ユーザー名</label>
+            <input class="registration-form__input" type="text" name="name" id="name">
+            <p class="registration-form__error-message">
+                @error('email')
+            {{ $message }}
+            @enderror
+            </p>
+        </div>
+
+        <div class="registration-form__group">
+            <label class="registration-form__label" for="email">メールアドレス</label>
+            <input class="registration-form__input" type="mail" name="email" id="email">
+            <p class="registration-form__error-message">
+                @error('email')
+            {{ $message }}
+            @enderror
+            </p>
+        </div>
+        <div class="registration-form__group">
+            <label class="registration-form__label" for="password">パスワード</label>
+            <input class="registration-form__input" type="password" name="password" id="password">
+            <p>
+            @error('password')
+            {{ $message }}
+            @enderror
+            </p>
+        </div>
+        <div class="registration-form__group">
+            <label class="confirmation__label" for="confirmation__password">確認用パスワード</label>
+            <input class="registration-form__input" type="password" name="password" id="password">
+            <p>
+            @error('password')
+            {{ $message }}
+            @enderror
+            </p>
+        </div>
+
+        <div class="registration__button">
+            <button class="form__button-submit" type="submit">登録する</button>
+        </div>
+        </form>
+        <div class="registration__link">
+            <a class="registration__button-submit" href="/auth/login">ログインはこちら</a>
+        </div>
+</div>
+@endsection
